0c0efe99aab8722e9b8dae95f6660000d2b0f90b
ReakUnusuall <honor.exe@list.ru>
2023-03-01 17:02:01 +0300

    доделал игровое окно и вставил заранее подготовленную картинку, настроил траекторию полёта шарика, его столкновение с обьектами, его размеры и цвет, добавил дощечку, настроил её цвет, размеры, столкновение с шариком и движение по оси x, сделал так, что при попадании шарика в  нижний край экрана происходит поражение с соответствующей надписей, аналогично сделана победа при окончании всех прямоугольников.

--- main.py
+++ main.py
@@ -16,4 +16,70 @@ ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ba
 dx, dy = 1, -1
 
 block_list = [pygame.Rect(10 + 140 * i, 10 + 80 * j, 125, 68) for i in range(10) for j in range(4)]
-color_list = [(rnd(90, 93), rnd(90, 92), rnd(90, 91)) for i in range(10) for j in range(4)]
+сolor_list = [(rnd(90, 93), rnd(90, 92), rnd(90, 91)) for i in range(10) for j in range(4)]
+
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+img = pygame.image.load('1.jpg').convert()
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    sc.blit(img, (0, 0))
+    [pygame.draw.rect(sc, color_list[color], block) for color, block in enumerate(block_list)]
+    pygame.draw.rect(sc, pygame.Color('blue'), paddle)
+    pygame.draw.circle(sc, pygame.Color('orange'), ball.center, ball_radius)
+
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
+    
+    if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
+        dx = -dx
+    
+    if ball.centery < ball_radius:
+        dy = -dy
+    
+    if ball.colliderect(paddle) and dy > 0:
+        dx, dy = detect_collision(dx, dy, ball, paddle)
+
+    hit_index = ball.collidelist(block_list)
+    if hit_index != -1:
+        hit_rect = block_list.pop(hit_index)
+        hit_color = color_list.pop(hit_index)
+        dx, dy = detect_collision(dx, dy, ball, hit_rect)
+    
+    if ball.bottom > HEIGHT:
+        print('Поражение')
+        exit()
+    elif not len(block_list):
+        print('Победа')
+        exit()
+    
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddle_speed
+    if key[pygame.K_RIGHT] and paddle.right < WIDTH:
+        paddle.right += paddle_speed
+    
+    pygame.display.flip()
+    clock.tick(fps)
